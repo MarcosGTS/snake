8e3adef88b5632764c66e4cc47a6f5be438ff634
MarcosGTS <marcosgtsodre@gmail.com>
2022-03-11 10:24:42 -0300

    add snake movement

--- script.js
+++ script.js
@@ -40,3 +40,36 @@ function displayGrid(grid) {
 }
 
 displayGrid(grid);
+
+class Snake {
+    constructor(x, y) {
+        //Initial direction
+        this.direction = {x: 1, y:0};
+
+        //List of positions (x, y)
+        this.body = [{x, y}]
+    }
+
+    move() {
+        let head = this.body[0];
+        const {x, y} = this.direction;
+
+        head.x += x;
+        head.y += y;
+
+        //moving rest of the body
+        this.body = this.body
+            .map((el, i, arr) => arr[i-1] || el);
+
+        return this.body;
+    }
+
+    // changeDirection()
+
+    // grow()
+}
+
+const snake = new Snake(0,0);
+setInterval(() => {
+    console.table(snake.move());
+}, 3000)
